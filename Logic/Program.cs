aeab914cc16591d4b5c6f4edfed6c06d0a1fdaa2
SpoorthyKR <spoorthyk400@gmail.com>
2020-04-02 20:06:53 -0400

    Cohort

---
+++ Logic/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Poco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace logic
+
+{
+    public class poco : Logic<Poco>
+    {
+        public Poco(IDataRepository<Poco> repository) :
+            base(repository)
+        { }
+
+        protected override void Verify(Poco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (Poco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                {
+                    exceptions.Add(new ValidationException(107, "Blank Major Fix it!!"));
+                }
+              
+            }
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(Poco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(Poco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
---
+++ Poco/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Poco
+{
+    [Table("Employee")]
+    public class Poco 
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Employee { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+        [Column("Completion_Percent")]
+        public byte? CompletionPercent { get; set; }
+        [Column("Time_Stamp")]
+
+        [NotMapped]
+        public byte[] TimeStamp { get; set; }
+
+
+    }
+}
